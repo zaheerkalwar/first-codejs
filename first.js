06ae6028355b6368f904da55bec327a82d02967d
zaheerkalwar <zaheerahmedkalwar91@gmail.com>
2025-06-11 13:21:37 +0500

    operations

---
+++ first.js
@@ -0,0 +1,54 @@
+// //arithematic operations
+// let a= 5;
+// let b= 2;
+// console.log("a+b=", a+b);
+// console.log("a-b=",a-b);
+// console.log("a*b=", a*b)
+// console.log("a/b=",a/b)
+// console.log("a=",a, "b=",b)
+// //modulus
+// console.log("a%b=",a%b)
+// //exponentiation
+// console.log("a**b", a**b)
+
+
+// const teacher={
+//     fullName: "Zaheer Ahmed",
+//     Period: "first",
+//     Class: 9,
+//     ispresent: true
+// }
+// teacher["Class"]= teacher["Class"]+1
+// console.log( teacher.Class)
+// console.log(teacher.Period)
+// console.log(teacher["fullName"])
+// console.log(teacher.ispresent)
+
+
+// //unary operators
+// let a= 5
+// let b= 2
+// a++
+// console.log("a=",a)
+// b--
+// console.log("b=", b)
+
+// let a=5
+// let b=3
+// console.log("a++=",a++)
+// console.log("a=",a)
+
+
+// Assignment Operatore
+
+// let a= 7
+// b= 9
+// a+=5
+// console.log("a=", a)
+
+//Comparison Operators
+
+let a = 5
+let b = 4;
+console.log("a==b", a==b)
+console.log("a!=b", a!=b)
